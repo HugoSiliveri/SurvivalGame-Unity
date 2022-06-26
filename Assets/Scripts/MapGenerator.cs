5aec3bd662f555ee4a01c5b4eb1964e09a090ad3
Hugo S <hugo.siliveri@etu.umontpellier.fr>
2022-06-26 20:19:55 +0200

    MapPreview du monde généré

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGenerator : MonoBehaviour {
+
+	public enum DrawMode {NoiseMap, ColourMap};
+	public DrawMode drawMode;
+
+	public const int mapChunkSize = 241;
+	public float noiseScale;
+
+	public int octaves;
+    [Range(0,1)]
+	public float persistance;
+	public float lacunarity;
+
+	public int seed;
+	public Vector2 offset;
+
+	public bool autoUpdate;
+
+	public TerrainType[] regions;
+
+	public void GenerateMap() {
+		float[,] noiseMap = Noise.GenerateNoiseMap (mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity, offset);
+
+		Color[] colourMap = new Color[mapChunkSize * mapChunkSize];
+		for (int y = 0; y < mapChunkSize; y++)
+        {
+			for (int x = 0; x < mapChunkSize; x++)
+            {
+				float currentHeight = noiseMap[x, y];
+				for (int i = 0; i < regions.Length; i++)
+                {
+					if (currentHeight <= regions[i].height)
+                    {
+						colourMap[y * mapChunkSize + x] = regions[i].colour;
+						break;
+                    }
+                }
+            }
+        }
+
+		MapDisplay display = FindObjectOfType<MapDisplay>();
+
+		if (drawMode == DrawMode.NoiseMap)
+        {
+			display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+		}
+		else if (drawMode == DrawMode.ColourMap)
+        {
+			display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapChunkSize, mapChunkSize));
+        }
+		
+	}
+
+    private void OnValidate()
+    {
+		if (lacunarity < 1)
+        {
+			lacunarity = 1;
+        }
+		if (octaves < 0)
+        {
+			octaves = 0;
+        }
+    }
+}
+
+[System.Serializable]
+public struct TerrainType
+{
+	public string name;
+	public float height;
+	public Color colour;
+}
